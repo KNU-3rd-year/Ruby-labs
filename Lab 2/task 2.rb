9cc874e34cfeeab167e279b6fb5135ffbe00a2bd
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-09-14 17:35:53 +0300

    Done: Lab 2 part 2

--- Lab 2/task 2.rb
+++ Lab 2/task 2.rb
@@ -1,1 +1,12 @@
+# frozen_string_literal: true
 
+puts 'type P:'
+p = gets.chomp.to_f
+puts 'type t:'
+t = gets.chomp.to_f
+puts 'type r:'
+r = gets.chomp.to_f
+
+diapason = p**r * (1 - 1 / p**t)
+
+print 'Diapason = ', diapason.round
