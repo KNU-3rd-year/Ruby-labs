58ca5fe13a888e89de463450a2d7b31e4eab6e20
Mark <yavorskiymark@gmail.com>
2022-09-29 22:38:25 +0300

    Done: lab 4 task 2

--- Lab 4/task 2.rb
+++ Lab 4/task 2.rb
@@ -5,6 +5,11 @@ n = 8
 ar_a = Array.new(n).map! { Array.new(n).map! { rand(50) } }
 ar_b = Array.new(n).map! { Array.new(n).map! { rand(50) } }
 
+(0..n-1).each do |i|
+  ar_a[i][i] = 1
+  ar_b[i][i] = 1
+end
+
 a = Matrix[*ar_a]
 b = Matrix[*ar_b]
 puts a.to_a.map(&:inspect), "\n"
