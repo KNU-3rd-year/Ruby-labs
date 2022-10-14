0f8a727216602dbdaa285d7cc10e0caf4cff6417
mark <yavorskiymark@gmail.com>
2022-10-14 22:05:16 +0300

    Done: lab 6

---
+++ Lab 6/README.md
@@ -0,0 +1,15 @@
+# Lab 6
+
+### Task:
+After entering the university, information was collected about the students: 
+surname, whether they need a hostel, experience, whether they worked as a teacher, what they graduated from, 
+what language they studied. 
+Draw up a program that determines:
+1. how many people need a hostel;
+2. lists of students who have worked as a teacher for 2 or more years; 
+3. lists of those who graduated from Stanford;
+4. language groups lists
+
+### Solution:
+
+[task 1.rb](task%201.rb)
---
+++ Lab 6/task 1.rb
@@ -0,0 +1,47 @@
+class Student
+  attr_accessor :name,
+                :need_hostel,
+                :experience,
+                :is_teacher,
+                :graduated_from,
+                :language_studied
+
+  def initialize(name, need_hostel, experience, is_teacher, graduated_from, language_studied)
+    @name = name
+    @need_hostel = need_hostel
+    @experience = experience
+    @is_teacher = is_teacher
+    @graduated_from = graduated_from
+    @language_studied = language_studied
+  end
+end
+
+def create_random_student
+  name = %w[Liam Olivia Noah Emma Oliver Charlotte].sample
+  need_hostel = [true, false].sample
+  experience = rand(5)
+  is_teacher = [true, false].sample
+  graduated_from = %w[Stanford MIT Carnegie Oxford NUS Harvard].sample
+  language_studied = %w[UKR ENG GER FRA CHN KOR].sample
+
+  Student.new(name, need_hostel, experience, is_teacher, graduated_from, language_studied)
+end
+
+n = 50
+students = Array.new(n).map! { create_random_student }
+students.each { |student| p student }
+
+puts "\nhow many people need a hostel"
+p students.count { |student| student.need_hostel }
+
+puts "\nlists of students who have worked as a teacher for 2 or more years:"
+students.find_all {|student| student.experience >= 2 }.each { |student| p student }
+
+puts "\nlists of those who graduated from Stanford:"
+students.find_all {|student| student.graduated_from == "Stanford" }.each { |student| p student }
+
+puts "\nlanguage groups lists:"
+students.group_by {|student| student.language_studied }.each do |language, group|
+  puts "\nStudied language group #{language}:"
+  group.each { |student| p student }
+end
